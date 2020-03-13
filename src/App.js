7f1cb564c649f3ff16cd0a1957ef119b7e2b0b8f
Marcel Balint <blank_blankness@yahoo.com>
2020-03-13 01:34:06 -0400

    Imported the Card component in the main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+
 import "./App.css";
+import Card from "./components/Card";
 
 class App extends Component {
   render() {
-    return <div className="container"></div>;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <Card add={this.addHandler} text="First" />
+          <Card add={this.addHandler} text="Second" />
+          <Card add={this.addHandler} text="Third" />
+          <Card add={this.addHandler} text="Fourth" />
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
